c60d45cc9870c19b07e8d9c38e8ec4dccc7a69fa
Kate <markelova.ee@phystech.edu>
2021-04-30 19:11:37 +0300

    learning how to use BoxLayout

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.image import Image, AsyncImage
+from kivy.uix.boxlayout import BoxLayout
 
 from kivy.core.window import Window
 
@@ -20,11 +21,16 @@ Window.clearcolor = (1, 1, 1, 1)
 
 class AcquaiNoteApp(App):  # create subclass of a kivy class
     def build(self):
-        img = Image(
-            source ='data/images/contact_image.png'
+        layout = BoxLayout()
+        btn = Button(
+            text="+"
         )
-        return img
-
+        btn2 = Button(
+            text="Menu"
+        )
+        layout.add_widget(btn)
+        layout.add_widget(btn2)
+        return layout
 
 
 if __name__ == '__main__':
