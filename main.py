c176d8ef75e75f7afd8453a2210d15c0d917ca78
Kate <markelova.ee@phystech.edu>
2021-04-30 23:37:00 +0300

    made a colored label for a starting screen

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
 import sys
 
-from kivy.app import App
+# from kivy.app import App
 
-# from kivymd.app import MDApp
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel, MDIcon
+from kivymd.uix.button import MDFloatingActionButton, MDFlatButton
+from kivymd.uix.screen import Screen
+from kivymd.font_definitions import theme_font_styles
+
 # from kivymd.theming import ThemeManager
 # from kivymd.navigationdrawer import NavigationDrawer
 
 # from kivy.uix.screenmanager import (ScreenManager, Screen, FadeTransition)
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.uix.label import Label
+
 from kivy.uix.image import Image, AsyncImage
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -22,12 +27,19 @@ Window.clearcolor = (1, 1, 1, 1)
 Window.size = (360, 600)
 
 
-class AcquaiNoteApp(App):  # create subclass of a kivy class
+class AcquaiNoteApp(MDApp):  # create subclass of a kivy class
     def build(self):
         layout = BoxLayout(
             orientation='vertical',
             spacing=10,
             padding=80
+        )
+        label = MDLabel(
+            text="AcquaiNote",
+            theme_text_color='Custom',
+            text_color=(0.416, 0.353, 0.804, 1),
+            font_style='H4',
+            halign='center'
         )
         img = Image(
             source='data/images/contact_image_b.png'
@@ -53,6 +65,8 @@ class AcquaiNoteApp(App):  # create subclass of a kivy class
             pos_hint={'center_x': 0.5},
             on_press=self.login
         )
+        layout.add_widget(icon_label)
+        layout.add_widget(label)
         layout.add_widget(img)
         layout.add_widget(login_layout)
         layout.add_widget(login_btn)
