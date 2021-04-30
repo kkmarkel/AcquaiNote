1afb6aa4b535230df46d5926802fa737e53e93e4
Kate <markelova.ee@phystech.edu>
2021-04-30 18:51:49 +0300

    learning how to add images

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from kivy.app import App
 
 # from kivy.uix.screenmanager import (ScreenManager, Screen, FadeTransition)
 from kivy.uix.button import Button
-# from kivy.uix.widget import Widget
+from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.uix.image import Image, AsyncImage
 
 from kivy.core.window import Window
 
@@ -19,29 +20,11 @@ Window.clearcolor = (1, 1, 1, 1)
 
 class AcquaiNoteApp(App):  # create subclass of a kivy class
     def build(self):
-        # label = Label(
-        #     text="AcquaiNote",
-        #     font_size='20sp',
-        #     bold=True,
-        #     italic=True,
-        #     color="#6A5ACD"
-        # )
-        button = Button(
-            text="+",
-            font_size="20sp",
-            pos_hint={'center_x': 0.5, 'center_y': 0.1},
-            size=(50, 50),
-            size_hint=(0.15, 0.15),
-            on_press=self.printpress,
-            on_release=self.printrelease
+        img = Image(
+            source ='data/images/contact_image.png'
         )
-        return button
+        return img
 
-    def printpress(self, obj):
-        print("Button has been pressed")
-
-    def printrelease(self, obj):
-        print("Button has been released")
 
 
 if __name__ == '__main__':
